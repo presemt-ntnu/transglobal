caece9981e510de1d3ec675d7a46e68aabaad3b6
Erwin Marsi <emarsi@idi.ntnu.no>
2012-03-19 20:39:59 +0100

    first version of de-en translation,including scoring

--- lib/tg/annot.py
+++ lib/tg/annot.py
@@ -106,3 +106,68 @@ class TreeTaggerEnglish(Annotator):
     
     
     
+class TreeTaggerGerman(Annotator):
+    """
+    annotation of German input text with TreeTagger 
+    """
+    
+    def __init__(self, command="tree-tagger-german"):
+        self.command = command
+        self.sent_end_tag = "$."
+        
+        
+    def annotate(self, text):
+        log.debug("TreeTagger input:\n" + text)
+        
+        # convert from unicode to Latin1 encoding
+        # we may loose some data here!
+        text = text.encode("latin1", "backslashreplace")
+        
+        # create pipe to tagger
+        log.debug("Calling TreeTagger as " + self.command)
+        tagger_proc = subprocess.Popen(self.command, shell=True,
+                                       stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                                       stderr=subprocess.PIPE)
+
+        # send text and retrieve tagger output 
+        tagger_out, tagger_err = tagger_proc.communicate(text)
+        
+        # and convert back from Latin1 to unicode
+        tagger_out = tagger_out.decode("latin1")        
+        log.debug("TreeTagger standard output:\n" + tagger_out)
+        log.debug("TreeTagger standard error:\n" + tagger_err)
+        
+        return tagger_out
+    
+    
+    def convert(self, tagger_out):
+        graph_list = []
+        make_new_graph = True
+        
+        for line in tagger_out.strip().split("\n"):
+            if make_new_graph:
+                graph_id = "{0:03}".format(len(graph_list) + 1)
+                log.info("creating graph " + graph_id)
+                graph = transgraph.TransGraph(id=graph_id)
+                make_new_graph = False
+                prev_node = None
+                
+            ### fix: TreeTagger for English sometimes produces output like
+            ### 'that\t\tIN\tthat',
+            ##line = line.replace("\t\t", "\t")                
+            word, tag, lemma = line.split("\t")
+            new_node = graph.add_source_node(word=word, tag=tag, lemma=lemma)
+            
+            if prev_node:
+                graph.add_word_order_edge(prev_node, new_node)
+            else:
+                graph.set_source_start_node(new_node)
+            
+            if tag == self.sent_end_tag:
+                graph_list.append(graph)
+                make_new_graph = True
+                prev_node = None
+            else:
+                prev_node = new_node
+                
+        return graph_list
---
+++ scratch/presemt-de-en.py
@@ -0,0 +1,80 @@
+"""
+evaluate de-en translation on Presemt evaluation data
+"""
+
+import logging
+import cPickle
+import xml.etree.cElementTree as et
+
+from tg.config import config
+from tg.annot import TreeTaggerGerman
+from tg.transdict import TransDict, DictAdaptor
+from tg.lookup import Lookup
+from tg.freqscore import FreqScore
+from tg.draw import Draw
+from tg.arrange import Arrange
+from tg.format import TextFormat, MtevalFormat
+from tg.eval import mteval
+from tg.utils import set_default_log
+
+
+# for logging to stderr in utf-8 use:
+set_default_log(level=logging.INFO)
+
+
+
+# get text from input source
+xml_tree = et.ElementTree(file=config["eval"]["presemt"]["de-en"]["src_fname"])
+text = " ".join(seg.text.strip() for seg in  xml_tree.iter("seg"))
+
+# annotate
+annotator = TreeTaggerGerman()
+graph_list = annotator(text)
+
+# lookup
+en_de_dict = DictAdaptor(config["dict"]["de-en"]["pkl_fname"],
+                         config["dict"]["de-en"]["posmap_fname"])
+lookup = Lookup(en_de_dict)
+lookup(graph_list)
+
+# frequency scoring
+freq_score = FreqScore(config["count"]["lemma"]["en"]["pkl_fname"])
+freq_score(graph_list)
+
+# draw
+draw = Draw()
+draw(graph_list, out_format="pdf")
+    
+# arrange 
+arrange = Arrange()
+arrange(graph_list)
+
+# save
+cPickle.dump(graph_list, open("graphs.pkl", "wb"))
+
+# graph_list = cPickle.load(open("graphs.pkl"))
+
+format = TextFormat()
+format(graph_list)
+format.write()
+
+# write translation output in Mteval format
+format = MtevalFormat(srclang="German", trglang="English",
+                      sysid="transglobal:most frequent lemma")
+format(graph_list)
+tst_fname = "out_de_en.tst"
+format.write(tst_fname)
+
+# calculate BLEU and NIST scores using mteval script
+mteval(config["eval"]["presemt"]["de-en"]["lemma_ref_fname"],
+       config["eval"]["presemt"]["de-en"]["src_fname"],
+       tst_fname,
+       "out_de_en.scores")
+
+
+
+
+
+
+
+
