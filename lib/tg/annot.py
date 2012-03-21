39e0a0f2ac5585769e1e24c38452da5e073889d7
Erwin Marsi <emarsi@idi.ntnu.no>
2012-03-21 13:51:51 +0100

    factored out common code for TreeTaggerEnglish and TreeTaggerGerman; added default option to replace unknown lemmas by word form

--- lib/tg/annot.py
+++ lib/tg/annot.py
@@ -37,22 +37,28 @@ class Annotator(graphproc.GraphProces):
         raise NotImplementedError
     
     
-class TreeTaggerEnglish(Annotator):
+    
+class TreeTagger(Annotator):
     """
-    annotation of English input text with TreeTagger 
+    annotation of nput text with TreeTagger 
     """
     
-    def __init__(self, command="tree-tagger-english"):
+    lemma_unknown = "<unknown>"
+    
+    def __init__(self, command, encoding, sent_end_tag,
+                 replace_unknown_lemma=True):
         self.command = command
-        self.sent_end_tag = "SENT"
+        self.encoding= encoding
+        self.sent_end_tag = sent_end_tag
+        self.replace_unknown_lemma = replace_unknown_lemma
         
         
     def annotate(self, text):
         log.debug("TreeTagger input:\n" + text)
         
-        # convert from unicode to Latin1 encoding
+        # convert from unicode to given encoding
         # we may loose some data here!
-        text = text.encode("latin1", "backslashreplace")
+        text = text.encode(self.encoding, "backslashreplace")
         
         # create pipe to tagger
         log.debug("Calling TreeTagger as " + self.command)
@@ -63,13 +69,12 @@ class TreeTaggerEnglish(Annotator):
         # send text and retrieve tagger output 
         tagger_out, tagger_err = tagger_proc.communicate(text)
         
-        # and convert back from Latin1 to unicode
-        tagger_out = tagger_out.decode("latin1")        
+        # and convert back from given encoding to unicode
+        tagger_out = tagger_out.decode(self.encoding)        
         log.debug("TreeTagger standard output:\n" + tagger_out)
         log.debug("TreeTagger standard error:\n" + tagger_err)
         
-        return tagger_out
-    
+        return tagger_out    
     
     def convert(self, tagger_out):
         graph_list = []
@@ -87,6 +92,11 @@ class TreeTaggerEnglish(Annotator):
             # 'that\t\tIN\tthat',
             line = line.replace("\t\t", "\t")                
             word, tag, lemma = line.split("\t")
+            
+            if lemma == self.lemma_unknown and self.replace_unknown_lemma:
+                log.warn(u"lemma unknown; using word {0} instead ".format(word))
+                lemma = word
+                        
             new_node = graph.add_source_node(word=word, tag=tag, lemma=lemma)
             
             if prev_node:
@@ -103,71 +113,31 @@ class TreeTaggerEnglish(Annotator):
                 
         return graph_list
 
+
+
+class TreeTaggerEnglish(TreeTagger):
+    """
+    annotation of English input text with TreeTagger 
+    """
     
-    
-    
-class TreeTaggerGerman(Annotator):
+    def __init__(self, command="tree-tagger-english", 
+                 encoding="latin1",
+                 sent_end_tag="SENT",
+                 *arg, **kwargs):
+        TreeTagger.__init__(self, command, encoding, sent_end_tag, 
+                            *args, **kwargs)
+  
+
+
+class TreeTaggerGerman(TreeTagger):
     """
     annotation of German input text with TreeTagger 
     """
     
-    def __init__(self, command="tree-tagger-german"):
-        self.command = command
-        self.sent_end_tag = "$."
-        
-        
-    def annotate(self, text):
-        log.debug("TreeTagger input:\n" + text)
-        
-        # convert from unicode to Latin1 encoding
-        # we may loose some data here!
-        text = text.encode("latin1", "backslashreplace")
-        
-        # create pipe to tagger
-        log.debug("Calling TreeTagger as " + self.command)
-        tagger_proc = subprocess.Popen(self.command, shell=True,
-                                       stdin=subprocess.PIPE, stdout=subprocess.PIPE,
-                                       stderr=subprocess.PIPE)
-
-        # send text and retrieve tagger output 
-        tagger_out, tagger_err = tagger_proc.communicate(text)
-        
-        # and convert back from Latin1 to unicode
-        tagger_out = tagger_out.decode("latin1")        
-        log.debug("TreeTagger standard output:\n" + tagger_out)
-        log.debug("TreeTagger standard error:\n" + tagger_err)
-        
-        return tagger_out
-    
-    
-    def convert(self, tagger_out):
-        graph_list = []
-        make_new_graph = True
-        
-        for line in tagger_out.strip().split("\n"):
-            if make_new_graph:
-                graph_id = "{0:03}".format(len(graph_list) + 1)
-                log.info("creating graph " + graph_id)
-                graph = transgraph.TransGraph(id=graph_id)
-                make_new_graph = False
-                prev_node = None
-                
-            ### fix: TreeTagger for English sometimes produces output like
-            ### 'that\t\tIN\tthat',
-            ##line = line.replace("\t\t", "\t")                
-            word, tag, lemma = line.split("\t")
-            new_node = graph.add_source_node(word=word, tag=tag, lemma=lemma)
-            
-            if prev_node:
-                graph.add_word_order_edge(prev_node, new_node)
-            else:
-                graph.set_source_start_node(new_node)
-            
-            if tag == self.sent_end_tag:
-                graph_list.append(graph)
-                make_new_graph = True
-                prev_node = None
-            else:
-                prev_node = new_node
-                
-        return graph_list
+    def __init__(self, command="tree-tagger-german", 
+                 encoding="latin1",
+                 sent_end_tag="$.",
+                 *arg, **kwargs):
+        TreeTagger.__init__(self, command, encoding, sent_end_tag,
+                            *arg, **kwargs)
+      
