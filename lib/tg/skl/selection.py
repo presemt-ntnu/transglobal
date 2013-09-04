c4c358dc4d6ad79cf4842adbe185d5c4f2cb4b65
Erwin Marsi <emarsi@idi.ntnu.no>
2013-09-04 11:29:29 +0200

    Rewrite frequency based feature selection to operate on document counts rather than absolute word counts.

--- lib/tg/skl/selection.py
+++ lib/tg/skl/selection.py
@@ -12,25 +12,25 @@ import numpy as np
 import scipy.sparse as sp
 
 from sklearn.base import BaseEstimator
-from sklearn.feature_selection.selector_mixin import SelectorMixin
+from sklearn.feature_selection.from_model import _LearntSelectorMixin
 
 log = logging.getLogger(__name__)
 
 
 
-class MinCountFilter(BaseEstimator, SelectorMixin):
+class MinCountFilter(BaseEstimator, _LearntSelectorMixin):
     """
-    Filter vocabulary terms with a count below the minimum count
+    Filter out all features (vocabulary terms) with an instance (document)
+    count below the minimum count threshold.
     """
     
-    # SelectorMixin provides 
+    # _LearntSelectorMixin provides 
     #     transform(self, X, threshold=None) 
     #     fit_transform(self, X, y=None, **fit_params)
     # and requires
     #     self.feature_importances_
     #     self.threshold (optional)
     #     self.fit(X, **fit_params)
-    #     self.transform(X)
     
     def __init__(self, min_count=5):
         self.min_count = min_count
@@ -38,10 +38,16 @@ class MinCountFilter(BaseEstimator, SelectorMixin):
     
     def fit(self, X, y=None, **fit_params):
         if sp.issparse(X):
-            # for sparse matrix, summation returns matrix
-            self.feature_importances_ = np.asarray(X.sum(axis=0))[0]
+            # Indices attrib of sparse matrix in csr format contains column
+            # numbers of non-zero elements. Counting the number of
+            # occurrences of each value in this array therefore gives the
+            # number of non-zero elements per column. The minlength argument
+            # guarantees even zero-count columns at the end of the matrix are
+            # included.
+            assert X.format == "csr"
+            self.feature_importances_ = np.bincount(X.indices, minlength=X.shape[1])
         else:
-            self.feature_importances_ = X.sum(axis=0)
+            self.feature_importances_ = (X > 0).sum(axis=0)
         
         log.debug("MinCountFilter removed out {} of {} features".format(
             np.sum(self.feature_importances_ < self.threshold),
@@ -51,25 +57,34 @@ class MinCountFilter(BaseEstimator, SelectorMixin):
         
         
         
-class MaxFreqFilter(BaseEstimator, SelectorMixin):
+class MaxFreqFilter(BaseEstimator, _LearntSelectorMixin):
     """
-    Filter vocabulary terms with a frequency above the maximum frequency
-    threshold, where frequency is simply the mean value of a feature
+    Filter out features (vocabulary terms) with a instance (document)
+    frequency above the maximum frequency threshold.
     """
     
-    def __init__(self, max_freq=.01):
+    def __init__(self, max_freq=.05):
         self.max_freq = max_freq
         self.threshold = 1
         
     def fit(self, X, y=None, **fit_params):
         if sp.issparse(X):
-            # for sparse matrix, summation returns matrix
-            means = np.asarray(X.mean(axis=0))[0]
+            # Indices attrib of sparse matrix in csr format contains column
+            # numbers of non-zero elements. Counting the number of
+            # occurrences of each value in this array therefore gives the
+            # number of non-zero elements per column. The minlength argument
+            # guarantees even zero-count columns at the end of the matrix are
+            # included.
+            assert X.format == "csr"
+            counts = np.bincount(X.indices, minlength=X.shape[1])
         else:
-            means = X.mean(axis=0)
-        
-        self.feature_importances_ = np.array(means <= self.max_freq, 
-                                             dtype=np.int) 
+            counts = (X > 0).sum(axis=0)
+            
+        max_count = self.max_freq * X.shape[0]
+        # All features with a count above max_count get become "0" whereas
+        # all others become "1". Using a threshold of "1", transform will
+        # then select the featurs with a count below or equal to max_count.
+        self.feature_importances_ = (counts <= max_count).astype("i")
         
         log.debug("MaxFreqFilter removed {} of {} features".format(
             X.shape[1] - self.feature_importances_.sum(),
