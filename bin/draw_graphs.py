07c079b563a963d81a82da8c92e90e87cf127f9a
Erwin Marsi <emarsi@idi.ntnu.no>
2013-09-18 11:45:48 +0200

    command line script for drawing translation graphs

---
+++ bin/draw_graphs.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Draw translation graphs
+"""
+
+import argparse
+import cPickle
+import logging
+import os
+
+from tg.draw import Draw
+
+
+log = logging.getLogger(os.path.basename(__name__))
+
+
+def draw_graphs(graphs_fname, out_format, i, j, best_score_attr,
+                base_score_attrs):
+    log.info("loading graphs from " + graphs_fname) 
+    graphs = cPickle.load(open(graphs_fname))  
+    
+    draw = Draw()
+    draw(graphs[i:j], out_format=out_format, best_score_attr=best_score_attr,
+         base_score_attrs=base_score_attrs)
+
+
+parser = argparse.ArgumentParser(description=__doc__)     
+    
+parser.add_argument(
+    "graphs_fname",
+    metavar="GRAPHS_FILE",
+    help="pickle file containing translation graphs")
+
+parser.add_argument(
+    "-i", "--from",
+    dest="i",
+    metavar="N",
+    type=int,
+    default=0,
+    help="first graph in range (counting from zero)")
+
+parser.add_argument(
+    "-j", "--to",
+    dest="j",
+    metavar="N",
+    type=int,
+    default=None,
+    help="last graph in range (counting from zero)")
+
+parser.add_argument(
+    "-f", "--format",
+    metavar="FORMAT",
+    default="pdf",
+    help="any drawing format supported by Graphviz including "
+    "pdf, ps, gif, png and jpeg (defaults to pdf)")
+
+parser.add_argument(
+    "-b", "--best-score-attr",
+    metavar="ATTR",
+    default="freq_score",
+    help="best score attribute (defaults to 'best_score')")
+
+parser.add_argument(
+    "-a", "--base-score-attrs",
+    metavar="ATTR",
+    nargs="+",
+    default=[],
+    help="base score attributes")
+
+
+args = parser.parse_args()
+
+draw_graphs(args.graphs_fname,
+            out_format=args.format,
+            best_score_attr=args.best_score_attr,
+            base_score_attrs=args.base_score_attrs,
+            i = args.i,
+            j = args.j)
